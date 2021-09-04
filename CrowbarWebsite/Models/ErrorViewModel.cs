9d35d43fd1052b7eff04aee750d1b7e271bca807
nzgamer41 <pslover9@gmail.com>
2021-09-04 14:11:25 +1200

    Add project files.

---
+++ CrowbarWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrowbarWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
