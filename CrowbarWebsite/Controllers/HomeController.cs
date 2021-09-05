ddd1c469d95139b13f39c014bee47980efd47ad9
Callum Herbert <cmh55@students.waikato.ac.nz>
2021-09-05 17:10:02 +1200

    *Added Model for Web Camera *Added XML Handling code *Added Leaflet JS Map Provider

--- CrowbarWebsite/Controllers/HomeController.cs
+++ CrowbarWebsite/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using CrowbarWebsite.Models;
+﻿using CrowbarWebsite.Helpers;
+using CrowbarWebsite.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -20,7 +21,25 @@ namespace CrowbarWebsite.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            //Download Static Camera XML
+            string xmlstr = "<?xml version=\"1.0\" encoding=\"utf-8\"?><static-cameras><camera><street>Twin Coast Discovery Highway </street><area>Twin Coast Discovery Highway </area><startpos><lat>-36.170245</lat><long>174.447012</long></startpos><endpos><lat>-36.190333</lat><long>174.450727</long></endpos><installed>Aug-2018</installed></camera></static-cameras>";
+
+            //Generate Camera Objects from XML
+            List<StaticCamera> cameras = new List<StaticCamera>();
+            using (var xmlr = XMLHelpers.CreateFromString(xmlstr))
+            {
+                StaticCamera camera = null;
+                do
+                {
+                    camera = StaticCamera.FromXML(xmlr);
+                    if (camera != null)
+                        cameras.Add(camera);
+
+                } while (camera != null);
+            }
+
+            //Pass to Page
+            return View(cameras);
         }
 
         public IActionResult Privacy()
---
+++ CrowbarWebsite/Helpers/XMLHelpers.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CrowbarWebsite.Helpers
+{
+    public static class XMLHelpers
+    {
+        public static Dictionary<string, string> ReadElement(this XmlReader xmlReader, string elementName) {
+            Dictionary<string, string> _els = new Dictionary<string, string>();
+            try
+            {
+                while (!(xmlReader.Name == elementName && xmlReader.NodeType == XmlNodeType.Element))
+                {
+                    xmlReader.Read();
+                    if (xmlReader.EOF)
+                        return new Dictionary<string, string>();
+                }
+                xmlReader.ReadStartElement(elementName);
+                string cnode = "", cval = "";
+                while (!(xmlReader.Name == elementName && xmlReader.NodeType == XmlNodeType.EndElement))
+                {
+                    switch (xmlReader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            cnode += (cnode.Length == 0 ? "" : ".") + xmlReader.Name;
+                            break;
+                        case XmlNodeType.Text:
+                            cval = xmlReader.Value;
+                            break;
+                        case XmlNodeType.EndElement:
+                            if (cval != "")
+                                _els.Add(cnode, cval);
+                            if (cnode.Contains("."))
+                                cnode = cnode.Substring(0, cnode.LastIndexOf("."));
+                            else
+                                cnode = "";
+                            cval = "";
+                            break;
+                        default:
+                            throw new NotImplementedException();
+                    }
+                    xmlReader.Read();
+
+                }
+                return _els;
+            }
+            catch { return new Dictionary<string, string>(); }
+        }
+
+        public static XmlReader CreateFromString(string data)
+        {
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(data));
+            var xmlr = XmlReader.Create(ms);
+            xmlr.Read();
+            return xmlr;
+        }
+    }
+}
