e115b0f9d52550ad44005f19ef67c0d3fe6a7cf4
nzgamer41 <pslover9@gmail.com>
2021-09-05 15:10:19 +1200

    s3 change

--- CrowbarWebsite/Controllers/HomeController.cs
+++ CrowbarWebsite/Controllers/HomeController.cs
@@ -4,11 +4,13 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml;
 using Amazon;
 using Amazon.S3;
+using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 
 namespace CrowbarWebsite.Controllers
@@ -16,7 +18,11 @@ namespace CrowbarWebsite.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private const string bucketName = "crowbar-staticdata";
+        private const string keyName = "static_cameras.xml";
+        // Specify your bucket region (an example region is shown).
+        private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast2;
+        private static IAmazonS3 client;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -24,7 +30,8 @@ namespace CrowbarWebsite.Controllers
 
         public IActionResult Index()
         {
-            GetS3Data();
+            client = new AmazonS3Client(bucketRegion);
+            ReadObjectDataAsync().Wait();
             return View();
         }
 
@@ -39,14 +46,38 @@ namespace CrowbarWebsite.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        static async Task GetS3Data()
+        static async Task ReadObjectDataAsync()
         {
-#if !DEBUG
-            TransferUtility fileTransfer = new TransferUtility(new AmazonS3Client(RegionEndpoint.USEast2));
-            fileTransfer.Download("/var/www/websiteapp/", "crowbar-staticdata", "static_cameras.xml");
-#else
-            Debug.WriteLine("Won't try to access S3 on debug build");
-#endif
+            string responseBody = "";
+            try
+            {
+                GetObjectRequest request = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = keyName
+                };
+                using (GetObjectResponse response = await client.GetObjectAsync(request))
+                using (Stream responseStream = response.ResponseStream)
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    string title = response.Metadata["x-amz-meta-title"]; // Assume you have "title" as medata added to the object.
+                    string contentType = response.Headers["Content-Type"];
+                    Console.WriteLine("Object metadata, Title: {0}", title);
+                    Console.WriteLine("Content type: {0}", contentType);
+
+                    responseBody = reader.ReadToEnd(); // Now you process the response body.
+                }
+                System.IO.File.WriteAllText("static_cameras.xml", responseBody);
+            }
+            catch (AmazonS3Exception e)
+            {
+                // If bucket or object does not exist
+                Console.WriteLine("Error encountered ***. Message:'{0}' when reading object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when reading object", e.Message);
+            }
         }
     }
 }
