5b7f4af5dde7c60ff22257a80db884e57db87584
Callum Herbert <cmh55@students.waikato.ac.nz>
2021-09-17 17:15:53 +1200

    + Added Loading Screen while building Cache Data + Moved Map web elements into a new View - Map.cshtml + Added Last Updated label to map * Cache Service now filters crash data to within ~100 meters each direction from camera.
    Note: This commit makes many major changes to the Home Controller and associated views.

---
+++ CrowbarWebsite/Helpers/CoordHelpers.cs
@@ -0,0 +1,17 @@
+﻿using Anywhere.ArcGIS.Common;
+using CrowbarWebsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowbarWebsite.Helpers
+{
+    public static class CoordHelpers
+    {
+        public static bool IsInBounds(this Point p, Coord camera, double radius)
+        {
+            return p.X >= camera.Longtitude - radius && p.X <= camera.Longtitude + radius && p.Y >= camera.Latitude - radius && p.Y <= camera.Latitude + radius;
+        }
+    }
+}
