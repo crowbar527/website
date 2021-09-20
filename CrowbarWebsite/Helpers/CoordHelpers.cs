aa923d6547e7e607a27d5e715469618e87bf6b3c
Callum Herbert <cmh55@students.waikato.ac.nz>
2021-09-20 20:35:58 +1200

    * Reworked code for adding map markers to page + Added in-bounds crash markers

--- CrowbarWebsite/Helpers/CoordHelpers.cs
+++ CrowbarWebsite/Helpers/CoordHelpers.cs
@@ -13,5 +13,10 @@ namespace CrowbarWebsite.Helpers
         {
             return p.X >= camera.Longtitude - radius && p.X <= camera.Longtitude + radius && p.Y >= camera.Latitude - radius && p.Y <= camera.Latitude + radius;
         }
+
+        public static List<Point> WhereInBounds(this List<Point> points, Coord camera, double radius)
+        {
+            return points.Where(x => x.IsInBounds(camera, radius)).ToList();
+        }
     }
 }
---
+++ CrowbarWebsite/Helpers/DisposableMapGenHelper.cs
@@ -0,0 +1,27 @@
+﻿using Anywhere.ArcGIS.Common;
+using CrowbarWebsite.Models;
+using CrowbarWebsite.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrowbarWebsite.Helpers
+{
+    public class DisposableMapGenHelper : IDisposable
+    {
+        private List<Point> _camPts;
+        public List<Point> CameraPoints => _camPts;
+
+        public DisposableMapGenHelper(StaticCamera sc, double radius = 0.001)
+        {
+            _camPts = CacheService.GetPointsForCamera(sc.GetInternalName()).WhereInBounds(sc.StartPos, radius);
+        }
+        public void Dispose()
+        {
+            
+        }
+
+        
+    }
+}
